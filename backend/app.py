cb6ee71f7658070301540caf14ae34d365abc375
Angel <cy2389@columbia.edu>
2017-04-16 16:25:31 -0400

    ibm watson service

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from tweet_listener import get_tweets
+from sentiment_analyzer import get_tweet_sentiments
+
+#@app.route('/getTwitterData/<username>', method=['POST'])
+def getTwitterData(username):
+
+    sentimentData = getSentiments(username);
+    return reformatForUI(sentimentData)
+
+def get_sentiment_list(username):
+
+    tweets = get_tweets(username)
+    return get_tweet_sentiments(tweets)
+
+print(get_sentiment_list('angel__yang'))
+
+
