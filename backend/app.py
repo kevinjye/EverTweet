68b8564c4335f27a9f92d61ecdd465693004decb
Angel <cy2389@columbia.edu>
2017-04-16 16:38:52 -0400

    flask set up

--- backend/app.py
+++ backend/app.py
@@ -3,17 +3,23 @@ from flask import Flask
 from tweet_listener import get_tweets
 from sentiment_analyzer import get_tweet_sentiments
 
-#@app.route('/getTwitterData/<username>', method=['POST'])
+app = Flask(__name__)
+app.config.update(
+    DEBUG=True,
+    SECRET_KEY='EVERTWEET'
+)
+
+@app.route('/getTwitterData/<username>', methods=['GET', 'POST'])
 def getTwitterData(username):
 
     sentimentData = getSentiments(username);
     return reformatForUI(sentimentData)
 
+@app.route('/getSentimentList/<username>', methods=['GET', 'POST'])
 def get_sentiment_list(username):
 
     tweets = get_tweets(username)
     return get_tweet_sentiments(tweets)
 
-print(get_sentiment_list('angel__yang'))
-
-
+if __name__ == '__main__':
+    app.run()
